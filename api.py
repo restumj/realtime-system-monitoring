1300c6c8504c169c7763f8aeb63c0f9740ba8cfb
restumj <anyrestu123@gmail.com>
2026-09-23 22:17:19 +0800

    make it to be a function

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 import json
 import psutil
 
-cpu_usage = psutil.cpu_percent(interval=1)
-ram = psutil.virtual_memory()
-data = {'cpu': cpu_usage, 'total_ram': ram.total/(1024**3), 'available_ram': ram.available/(1024**3), 'used_ram': ram.used/(1024**3), 'ram_usage_percentage': ram.percent}
-with open('api.json','w') as f:
-    json.dump(data,f,indent=4)
+def crete_data_json():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory()
+    data = {'cpu': cpu_usage, 'total_ram': ram.total/(1024**3), 'available_ram': ram.available/(1024**3), 'used_ram': ram.used/(1024**3), 'ram_usage_percentage': ram.percent}
+    with open('api.json','w') as f:
+        json.dump(data,f,indent=4)
