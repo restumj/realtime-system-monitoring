08d10c876dafffc1824ad1cf1975291109f69599
restumj <anyrestu123@gmail.com>
2026-09-25 12:40:18 +0800

    more styling: adding details to device spesification and what is displayed

--- static/script.js
+++ static/script.js
@@ -2,8 +2,11 @@ async function updateStats() {
     const response = await fetch('/api/stats');
     const data = await response.json();
 
-    document.getElementById('cpu').innerText = data.cpu_usage;
-    document.getElementById('ram').innerText = data.used_ram;
+    document.getElementById('cpu_usage').innerText = "Usage: " + data.cpu_usage;
+    document.getElementById('ram-total').innerText = "Total: " + data.total_ram;
+    document.getElementById('ram-available').innerText = "Available: " + data.available_ram;
+    document.getElementById('ram-used').innerText = "Used: " + data.used_ram;
+    document.getElementById('ram-usage').innerText = "Usage: " + data.ram_usage;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
